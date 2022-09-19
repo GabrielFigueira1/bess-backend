9ecc6257e13312a0fb7f58f87870f9b3675afe4d
Gabriel Figueira <gabrielfigueira98@gmail.com>
2022-09-19 00:27:46 -0400

    Add handles to websocket

--- app-ws.js
+++ app-ws.js
@@ -1,19 +1,50 @@
 // websockets setup
-const WebSocket = require('ws');
- 
+const WebSocket = require("ws");
+var users = [];
 function onError(ws, err) {
-    console.error(`onError: ${err.message}`);
+    console.error("onError: ${err.message}");
 }
- 
+
+function sendToDashboard(message){
+    if (users["Dashboard"] != undefined)
+        users["Dashboard"].send(message);
+    else
+        console.error("Dashboard not connected yet");
+}
+
+function sendToMCU(message){
+    if (users["MCU"] != undefined)
+        users["MCU"].send(message);
+    else
+        console.error("MCU not connected yet");
+}
+
 function onMessage(ws, data) {
     console.log(`onMessage: ${data}`);
-    ws.send(`recebido!`);
+    if (data.includes("ocv")){ //onMessage: 3.890000;10.000000;75.429817;53363.00 //payload
+        console.log("Payload received!");
+        let message = JSON.parse(data);
+        message = JSON.stringify(message);
+        sendToDashboard(message);
+
+        ws.send("Payload received!");
+    }
+    else if (data.includes("userName")){ //metadata
+        let message = JSON.parse(data);
+        users[message.userName] = ws;
+
+        console.log("User connected. User name: " + message.userName);
+        console.log("Current user count " + Object.keys(users).length);
+
+        ws.send("User connected. Hello " + message.userName + " from server");
+    }
 }
  
-function onConnection(ws, req) {
-    ws.on('message', data => onMessage(ws, data));
-    ws.on('error', error => onError(ws, error));
-    console.log(`onConnection`);
+function onConnection(ws, req, client) {
+    ws.on("message", data => onMessage(ws, data));
+    ws.on("error", error => onError(ws, error));
+    //console.log(`Received message from user ${ws.id}`);
+    console.log("onConnection");
 }
  
 module.exports = (server) => {
@@ -21,8 +52,8 @@ module.exports = (server) => {
         server
     });
  
-    wss.on('connection', onConnection);
+    wss.on("connection", onConnection);
  
-    console.log(`App Web Socket Server is running!`);
+    console.log("App Web Socket Server is running!");
     return wss;
 }
